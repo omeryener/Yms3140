6f689e9b3535c14a19a06d5a4d00fca54d72e379
fatih günalp <fatih.gunalp@outlook.com>
2020-08-24 13:49:48 +0300

    Proje oluşturuldu.

---
+++ OOP/24.08/WFA_Constructor/WFA_Constructor/Bebek.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFA_Constructor
+{
+    public class Bebek
+    {
+
+        //Not: Yapıcı metotlar class içerisinde görünmezler.
+
+        //Eğer bir class içerisinde yapıcı metot'a müdahele etmek istiyorsanız class ile aynı isme sahip bir metot tanımlamalısınız. Constructor'ların geri dönüş tipi olmaz.
+
+        //bir metotun başına 3 adet /// eklenirse o metot ile ilgili summary (özet ) oluşturur. Bu sayede metot kullanımını tooltipte görebiliriz.
+
+        /// <summary>
+        /// eğer boş bırakırsan sadece doğum tarihi eklenir.
+        /// </summary>
+        public Bebek()
+        {
+            DogumTarihi = DateTime.Now;
+        }
+        /// <summary>
+        /// string tipte değer gönderilirse ebeAdi olarak kaydedilir.
+        /// </summary>
+        /// <param name="ebeAdi"></param>
+        public Bebek(string ebeAdi)
+        {
+            DogumTarihi = DateTime.Now;
+            Ebe = ebeAdi;
+        }
+        /// <summary>
+        /// string tipte ebeAdi, string tipte anne adı gönderebilirsin.
+        /// </summary>
+        /// <param name="ebeAdi"></param>
+        /// <param name="anneAdi"></param>
+        public Bebek(string ebeAdi,string anneAdi)
+        {
+            DogumTarihi = DateTime.Now;
+            Ebe = ebeAdi;
+            AnneAdi = anneAdi;
+        }
+        public Bebek(string ebeAdi, string anneAdi, string babaAdi)
+        {
+            DogumTarihi = DateTime.Now;
+            Ebe = ebeAdi;
+            AnneAdi = anneAdi;
+            BabaAdi = babaAdi;
+        }
+
+        public Bebek(string ebeAdi, string anneAdi, string BabaAdi, string cinsiyet)
+        {
+            DogumTarihi = DateTime.Now;
+            Ebe = ebeAdi;
+            this.BabaAdi = BabaAdi;
+            Cinsiyet = cinsiyet;
+        }
+
+        public DateTime DogumTarihi { get; set; }
+        public string Ebe { get; set; }
+        public string AnneAdi { get; set; }
+        public string BabaAdi { get; set; }
+        public string Cinsiyet { get; set; }
+
+    }
+}
---
+++ OOP/24.08/WFA_Constructor/WFA_Constructor/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_Constructor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Constructor (Yapıcı Metot)
+
+            /*
+                Yapıcı metotlardan kısaca bahsetmek gerekirse , static olmayan (ileride göreceğiz) bir sınıf içerisindeki özelliklere sınıfın instance alımı esnasında değerleri atama işlemidir.
+             */
+
+            Bebek bebek1 = new Bebek();
+            bebek1.AnneAdi = "Hatice";
+            bebek1.BabaAdi = "Hüseyin";
+            bebek1.Cinsiyet = "Erkek";
+
+            bebek1.Ebe = "Hicran";
+
+            Bebek bebek2 = new Bebek();
+            bebek2.AnneAdi = "Fatma";
+            bebek2.BabaAdi = "Hasan";
+            bebek2.Cinsiyet = "Kız";
+            //bebek2.DogumTarihi = DateTime.Now;
+            bebek2.Ebe = "Merve";
+
+            Bebek bebek3 = new Bebek();
+            bebek3.AnneAdi = "Ayşe";
+            bebek3.BabaAdi = "Mehmet";
+            bebek3.Cinsiyet = "Kız";
+            //bebek3.DogumTarihi = DateTime.Now;
+            bebek3.Ebe = "Zeynep";
+
+            Bebek bebek4 = new Bebek("Şeyma");
+
+
+            //MessageBox.Show(bebek1.DogumTarihi.ToString());
+            //MessageBox.Show(bebek2.DogumTarihi.ToString());
+            //MessageBox.Show(bebek3.DogumTarihi.ToString());
+
+            //MessageBox.Show(bebek4.Ebe);
+
+            Bebek bebek5 = new Bebek("Merve", "Fatma", "Murat", "erkek");
+
+
+
+            //Object Initializer
+            Bebek bebek6 = new Bebek()
+            {
+                AnneAdi = "Zeynep",
+                BabaAdi = "Ahmet",
+                Ebe = "Ayşe",
+                Cinsiyet = "Kız",
+                DogumTarihi = DateTime.Now
+            };
+
+            Bebek bebek7 = new Bebek();
+            bebek7.AnneAdi = "Zeynep";
+            bebek7.BabaAdi = "Ahmet";
+
+        }
+    }
+}
